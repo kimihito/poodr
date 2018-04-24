b9210b62fddcccd1b60c1558c138092a0a2e56a3
kimihito <tatsurotamashiro@gmail.com>
2018-04-24 21:51:27 +0900

    Chapter 3

--- main.rb
+++ main.rb
@@ -1,31 +1,33 @@
 class Gear
-  attr_reader :chainring, :cog, :wheel
-
-  def initialize(chainring, cog, wheel=nil)
+  attr_reader :chainring, :cog
+  def initialize(chainring, cog)
     @chainring = chainring
     @cog = cog
-    @wheel = wheel
   end
-
   def ratio
     chainring / cog.to_f
   end
 
-  def gear_inches
-    ratio * wheel.diameter
+  def gear_inches(diameter)
+    ratio * diameter
   end
 end
 
 class Wheel
-  attr_reader :rim, :tire
+  attr_reader :rim, :tire, :gear
 
-  def initialize(rim, tire)
+  def initialize(rim, tire, chainring, cog)
     @rim = rim
     @tire = tire
+    @gear = Gear.new(chainring, cog)
   end
 
   def diameter
     rim + (tire * 2)
+  end
+
+  def gear_inches
+    gear.gear_inches(diameter)
   end
 
   def circumference
@@ -33,8 +35,4 @@ class Wheel
   end
 end
 
-@wheel = Wheel.new(26, 1.5)
-puts @wheel.circumference
-
-puts Gear.new(52, 11, @wheel).gear_inches
-puts Gear.new(52, 11).ratio
+puts Wheel.new(26, 1.5, 52, 11).gear_inches
